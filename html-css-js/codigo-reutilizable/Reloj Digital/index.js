dba43a46a8c6cb2851e4c06f94a319222965b5aa
Daniel Calderon <ddaniel_calderon@hotmail.com>
2023-05-02 23:24:34 -0300

    Update 02-04-2023

---
+++ html-css-js/codigo-reutilizable/Reloj Digital/index.js
@@ -0,0 +1,8 @@
+import { digitalClock, alarm } from "./reloj.js"
+
+const d = document
+
+d.addEventListener('DOMContentLoaded', (e) => {
+	digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj')
+	alarm('./assets/Send-It.mp3', '#activar-alarma', '#desactivar-alarma')
+})
