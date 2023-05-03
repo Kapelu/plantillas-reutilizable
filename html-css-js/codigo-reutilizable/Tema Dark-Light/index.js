d6d76d4198e3efb64a94a216e3e927ad646f660d
Daniel Calderon <ddaniel_calderon@hotmail.com>
2023-05-03 20:42:27 -0300

    Update 03-04-2023

--- html-css-js/codigo-reutilizable/Tema Dark-Light/index.js
+++ html-css-js/codigo-reutilizable/Tema Dark-Light/index.js
@@ -1,7 +1,3 @@
 import darkTheme from "./theme-dark.js"
 
-const d = document
-
-d.addEventListener('DOMContentLoaded', (e) => {
-	darkTheme('.dark-theme-btn', 'dark-mode')
-})
+darkTheme('.dark-theme-btn', 'dark-mode')
--- html-css-js/codigo-reutilizable/Tema Dark-Light/theme-dark.js
+++ html-css-js/codigo-reutilizable/Tema Dark-Light/theme-dark.js
@@ -1,21 +1,39 @@
 const d = document
-export default function darkTheme(btn, classDark){
-	const $themeBtn = d.querySelector(btn)
-	const $selectors = d.querySelectorAll('[data-dark]') 
-    console.log($selectors);
+const ls = localStorage
 
-	let moon='🌙', sun='☀️'
+export default function darkTheme(btn, classDark) {
+	const $themeBtn = d.querySelector(btn)
+	const $selectors = d.querySelectorAll('[data-dark]')
+	console.log($selectors)
+
+	let moon = '🌙'
+	let sun = '☀️'
 	console.log($themeBtn.textContent)
-	
-	d.addEventListener('click', e=>{
-		if(e.target.matches(btn)){
-			if($themeBtn.textContent === moon){
-				$selectors.forEach(el=>el.classList.add(classDark))
-				$themeBtn.textContent = sun
+
+	const ligthMode = () => {
+		$selectors.forEach((el) => el.classList.remove(classDark))
+		$themeBtn.textContent = moon
+		ls.setItem('theme', 'light')
+	}
+	const darkMode = () => {
+		$selectors.forEach((el) => el.classList.add(classDark))
+		$themeBtn.textContent = sun
+		ls.setItem('theme', 'dark')
+	}
+
+	d.addEventListener('click', (e) => {
+		if (e.target.matches(btn)) {
+			if ($themeBtn.textContent === moon) {
+				darkMode()
 			} else {
-				$selectors.forEach(el=>el.classList.remove(classDark))
-				$themeBtn.textContent = moon
+				ligthMode()
 			}
 		}
+	})
+	/* Pregunta al localStorage si existe una variable que controla el tema y en base a eso aplique los estilos correspondiente */
+	d.addEventListener('DOMContentLoaded', (e) => {
+		if (ls.getItem('theme') === null) ls.setItem('theme', 'light')
+		if (ls.getItem('theme') === 'light') ligthMode()
+		if (ls.getItem('theme') === 'dark') darkMode()
 	})
 }
