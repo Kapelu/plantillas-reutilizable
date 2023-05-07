684bd67ad3a233a311aef96d1d4c61d03610d58f
Daniel Calderon <ddaniel_calderon@hotmail.com>
2023-05-07 00:21:04 -0300

    Update 07-04-2023

---
+++ html-css-js/Código Reutilizable/Responsive Slider/index.js
@@ -0,0 +1,9 @@
+/* import { digitalClock, alarm } from "./reloj.js"
+
+const d = document
+
+d.addEventListener('DOMContentLoaded', (e) => {
+	digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj')
+	alarm('./assets/Send-It.mp3', '#activar-alarma', '#desactivar-alarma')
+})
+ */
